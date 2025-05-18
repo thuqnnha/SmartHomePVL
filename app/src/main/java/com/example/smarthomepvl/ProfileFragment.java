9ab506221b99da78848dec0e857a182a2daec389
thuan <haducthuan2003cpqn@gmail.com>
2025-05-18 22:53:52 +0700

    cap nhat giao dien

--- app/src/main/java/com/example/smarthomepvl/ProfileFragment.java
+++ app/src/main/java/com/example/smarthomepvl/ProfileFragment.java
@@ -4,11 +4,23 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 public class ProfileFragment extends Fragment {
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        // Lấy username từ Intent của Activity
+        String username = getActivity().getIntent().getStringExtra("username");
+
+        // Gán vào TextView
+        TextView appName = view.findViewById(R.id.appName);
+        appName.setText("Hello, "+username);
+
+        return view;
     }
 }
+
